c361ef67305110d366ab9e054f647e06c876df6b
hui <1549591817@qq.com>
2020-01-10 15:34:32 +0800

    初始化

---
+++ src/AppRouter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class AppRouter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div>hello</div>
+         );
+    }
+}
+ 
+export default AppRouter;
